17a39514ca1cde9884dd849dfdeb15910c343fbf
Jerry S Joseph <jerin.sebastian153@gmail.com>
2020-09-23 12:50:27 +0530

    Updates

---
+++ precache-manifest.67d6d919563eed1b6b3efbc9dc222bb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc0110ab41216c2f4a8e9411c946073",
+    "url": "/React_QuizApp_Client/index.html"
+  },
+  {
+    "revision": "c99efbca2e0396166f6b",
+    "url": "/React_QuizApp_Client/static/css/main.ab5731f3.chunk.css"
+  },
+  {
+    "revision": "219c267a7b4d9e4ebbdd",
+    "url": "/React_QuizApp_Client/static/js/2.12ab7c4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_QuizApp_Client/static/js/2.12ab7c4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99efbca2e0396166f6b",
+    "url": "/React_QuizApp_Client/static/js/main.70018f54.chunk.js"
+  },
+  {
+    "revision": "198d9837e6d05b4ac48e",
+    "url": "/React_QuizApp_Client/static/js/runtime-main.021c3718.js"
+  },
+  {
+    "revision": "35ff874327ff4876aa04b887e6a84afc",
+    "url": "/React_QuizApp_Client/static/media/star.35ff8743.png"
+  }
+]);
